b309c181bdea9c5b9d666ecc9f3c7690f51c379c
Narek Ghazaryan <nar.ghazaryan.dev@gmail.com>
2022-11-15 13:14:09 +0400

    Logger with static 2 lines of text :D

---
+++ exclusive_file/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <sys/fcntl.h>
+#include <unistd.h>
+
+int main() {
+    int logFD = open("./log.txt", O_WRONLY);
+
+    if(logFD < 0) {
+        perror("Chbacvav baxtd");
+        return 0;
+    }
+
+    char* l1 = "First line\n";
+    int res = write(logFD, l1, strlen(l1));
+
+    if(res < 0){
+        perror("Chgrvav baxtd");
+        return 0;
+    }
+
+    int logFDDup = dup(logFD);
+
+    if(logFDDup < 0){
+        perror("Chdupvav, bayc baxtd tut neprichom");
+        return 0;
+    }
+
+    char* l2 = "Second line\n";
+    res = write(logFDDup, l2, strlen(l2));
+
+    if(res < 0){
+        perror("Sax ereler, write ynchi cherav?");
+        return 0;
+    }
+
+    return 0;
+}
